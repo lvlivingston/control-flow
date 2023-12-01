bb77967c9b7b1701928f82fbc55c00203463bb46
lvlivingston <lvlivingston@gmail.com>
2023-12-01 15:56:10 -0600

    completed lesson

--- main.py
+++ main.py
@@ -1,2 +1,52 @@
 # Control Flow in Python ... to add a comment, cmd + /
 
+# floor = "sticky"
+# walls = "clean"
+
+# if floor == "sticky":
+#     print("Clean the floor! It's sticky!")
+# if walls == "sticky":
+#     print("Clean the walls! They're sticky!")
+# else:
+#     print("The walls are clean!")
+
+# color = input('Enter "green", "yellow", "red": ').lower()
+# print(f'The user entered {color}')
+
+# if color == "green":
+#     print("Go!")
+# elif color == "yellow":
+#     print("Slow down!")
+# elif color == "red":
+#     print("Stop!")
+# else:
+#     print("Bogus!")
+
+# names = ["Tom", "Deborah", "Murry", "Axel"]
+
+# for name in names:
+#     print(name)
+
+# color = ""
+# while True:
+#     color = input('Enter "green", "yellow", "red": ').lower()
+#     print(f'The user entered {color}')
+
+#     if color == "quit":
+#         break
+
+#     if color == "green":
+#         print("Go!")
+#     elif color == "yellow":
+#         print("Slow down!")
+#     elif color == "red":
+#         print("Stop!")
+#     else:
+#         print("Bogus!")
+
+# for num in range(5):
+#     print(num)
+
+# for even in range(2, 10, 2):
+#     print(even)
+
